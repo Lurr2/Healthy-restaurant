7dbf0fd194ec7c6c27f31128d538e87dba16d879
Lurr2 <jkasperski1995@gmail.com>
2022-06-29 10:44:55 +0200

    about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import data from '../../data.json';
 import AboutCard from '../common/card/AboutCard/AboutCard';
+import classes from './About.module.scss';
 
 const About = () => {
 
     return (
-        <section>
-            {data.map(AboutData => (
+        <section className={classes.container}>
+            <div className={classes.left}>
+                <h4>Naglowek</h4>
+                <p>Some dummy text for obtaining space in the future. Some dummy text for obtaining space in the future. Some dummy text for obtaining space in the future. Some dummy text for obtaining space in the future.</p>
+            </div>
+            <div className={classes.right}>
+               {data.map(element => (
                 <AboutCard 
-                key={AboutData.id}
-                text={AboutData.text}
-                title={AboutData.title}
-                team={AboutData.team}
-                join={AboutData.join}
-                ethic={AboutData.ethic}
-                goals={AboutData.goals}
+                key={element.id}
+                title={element.title}
+                team={element.team}
+                joinus={element.joinus}
+                ethic={element.ethic}
+                goals={element.goals}
                 />
-            ))}
-            
+               ))}
+            </div>
             
         </section>
     );
